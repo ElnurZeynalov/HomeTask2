8a89b2677992557e5c3e2458af5c8ef1a006d148
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-05 02:08:04 +0400

    Task1 Complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("Yoxlamaq isdediyiniz reqemi daxil edin: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x%2 == 0)
+            {
+                while (x>1)
+                {
+                    x=x/2;
+                    
+                }
+                if (x == 1)
+                { 
+                Console.WriteLine("Ikinin quvvetidir");
+                }
+                else 
+                { 
+                    Console.WriteLine("Ikinin quveti deyil");
+                }
+            }
+            else if (x == 1)
+            {
+                Console.WriteLine("Ikinin quvetidir");
+            }
+            else
+            {
+                Console.WriteLine("Ikinin quvveti deyil");
+            }
+        }
+    }
+}
