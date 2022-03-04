9c7c809e8773fe20c7161879ecb892361df549d5
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-05 02:08:29 +0400

    Task2 Complete

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Axdaracaginiz herfi daxil edin");
+            string herf =Console.ReadLine();
+            Console.WriteLine("Herfi axdaracagimiz soz veya cumleni daxil edin");
+            string soz = Console.ReadLine();
+            int total=0;
+            for (int i=0; i < soz.Length; i++)
+            {
+                if (herf.Contains(soz[i]))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
