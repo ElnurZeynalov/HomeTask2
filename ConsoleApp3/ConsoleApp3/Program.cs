787424a043f4378405f74c22633b88186859822b
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-05 02:08:46 +0400

    Task3 Complete

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,4,6,9,12,15,18,20,21};
+            int total=0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                if (arr[i]%3 == 0)
+                {  
+                    total =total+arr[i];
+                    
+                }
+            }
+            Console.WriteLine("Bu arrayin icindeki 3 e bolunen reqemlerin cemi "+ total+" beraberdir");
+        }
+    }
+}
